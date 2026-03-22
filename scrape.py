69f9d5070f6c13c83790d4902c9c9bcb7d528bf7
0RBalaji <rajalbandi2balaji@gmail.com>
2026-03-22 22:52:33 +0530

    API applied

--- scrape.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-payload = { 'api_key': 'e639d1fa0eef650e618d3fd4c29b2a8e', 'url': 'https://www.amazon.in/', 'render': 'true' }
-r = requests.get('https://api.scraperapi.com/', params=payload)
-print(r.text)
